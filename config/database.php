9bb4bada9b3ceea70003cde8c0ff152b61154ab5
Isaac White <iwhite@vivial.net>
2017-08-04 13:05:48 -0500

    One more change to get environment variables into code.

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+if (!defined('RDS_HOSTNAME')) {
+    define('RDS_HOSTNAME', $_SERVER('RDS_HOSTNAME'));
+    define('RDS_PORT', $_SERVER('RDS_PORT'));
+    define('RDS_DB_NAME', $_SERVER('RDS_DB_NAME'));
+    define('RDS_USERNAME', $_SERVER('RDS_USERNAME'));
+    define('RDS_PASSWORD', $_SERVER('RDS_PASSWORD'));
+}
+
 return [
 
     /*
