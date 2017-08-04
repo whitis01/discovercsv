b64077e0ff4f81b20891826b03c7230c8898de14
Isaac White <isaacwhite@Isaacs-MacBook-Pro.local>
2017-08-04 12:52:38 -0500

    Adding relevant commits to get AWS off the ground.

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', RDS_HOSTNAME),
+            'port' => env('DB_PORT', RDS_PORT),
+            'database' => env('DB_DATABASE', RDS_DB_NAME),
+            'username' => env('DB_USERNAME', RDS_USERNAME),
+            'password' => env('DB_PASSWORD', RDS_PASSWORD),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
